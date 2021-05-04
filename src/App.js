a5e816bcece5b424128c0fca3f67ddf36f6eb8fc
kosuke-17 <tamutamu17@icloud.com>
2021-05-04 12:02:01 +0900

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      counter:1,
+      msg:'count start',
+      flg:true,
+    }
+    this.doAction = this.doAction.bind(this)
+  }
+
+  doAction(e){
+    this.setState({
+      counter: this.state.counter + 1,
+      msg: this.state.counter,
+      flg: !this.state.flg
+    })
+  }
+
+  render(){
+    return <div>
+      <h1 className="bg-primary text-white display-4">React</h1>
+      <div className="container">
+        <p className="subtitle">Count number.</p>
+        {this.state.flg ?
+        <div className="alert alert-primary text-right">
+          <p className="h5">count: {this.state.msg}</p>
+        </div>
+        :
+        <div className="alert alert-warning text-left">
+          <p className="h5">{this.state.msg}</p>
+        </div>
+      }
+        <div className="alert alert-primary text-center">
+          <p className="h5 mb-4">{this.state.msg}</p>
+          <button className="btn btn-primary" onClick={this.doAction}>
+            Click</button>
+        </div>
+      </div>
+    </div>
+  }
+}
+
+export default App;
